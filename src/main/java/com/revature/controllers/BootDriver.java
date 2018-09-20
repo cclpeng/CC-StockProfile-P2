5a36c4ebb7db6bdc5eab450147cadd68d3973b7a
Cindy <cpeng3030@gmail.com>
2018-09-20 16:45:33 -0700

    redirection to angular app

--- src/main/java/com/revature/controllers/BootDriver.java
+++ src/main/java/com/revature/controllers/BootDriver.java
@@ -3,7 +3,7 @@ package com.revature.controllers;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
+@SpringBootApplication
 public class BootDriver {
 
 	public static void main(String[] args) {
--- src/main/java/com/revature/controllers/HomeController.java
+++ src/main/java/com/revature/controllers/HomeController.java
@@ -4,6 +4,7 @@ import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 public class HomeController {
@@ -13,5 +14,13 @@ public class HomeController {
 	public String displayMessage() {
 		return "Hello from our CC Boot App";
 	}
-
+	
+	@GetMapping(value="/login")
+	public RedirectView loginPage() {
+		//hehehhhh  :https://stackoverflow.com/questions/17955777/redirect-to-an-external-url-from-controller-action-in-spring-mvc
+		RedirectView redirectView = new RedirectView();
+	    redirectView.setUrl("http://com.revature.cireycindystock.s3-website-us-east-1.amazonaws.com/");
+	    return redirectView;
+	}
+	
 }

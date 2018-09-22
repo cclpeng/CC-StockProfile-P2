0dc31892cfcb398291005d33bae4455bc5b200a4
Cindy <cpeng3030@gmail.com>
2018-09-22 12:16:18 -0700

    trying

--- src/main/java/com/revature/controllers/HomeController.java
+++ src/main/java/com/revature/controllers/HomeController.java
@@ -3,6 +3,8 @@ package com.revature.controllers;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.view.RedirectView;
 
@@ -19,7 +21,18 @@ public class HomeController {
 	public RedirectView loginPage() {
 		//hehehhhh  :https://stackoverflow.com/questions/17955777/redirect-to-an-external-url-from-controller-action-in-spring-mvc
 		RedirectView redirectView = new RedirectView();
-	    redirectView.setUrl("http://com.revature.cireycindystock.s3-website-us-east-1.amazonaws.com/");
+	    redirectView.setUrl("http://cc-stockprofile-p2.com.s3-website-us-east-1.amazonaws.com/login");
+	    return redirectView;
+	}
+	
+	@PostMapping(value="/login", consumes=MediaType.TEXT_PLAIN_VALUE, produces=MediaType.TEXT_PLAIN_VALUE)
+	public RedirectView loginValidationRedirection(@RequestBody String username)
+	{
+		System.out.println(username); //see if we can get those params :DD
+		//set up a session with those params
+		RedirectView redirectView = new RedirectView();
+//	    redirectView.setUrl("http://com.revature.cireycindystock.s3-website-us-east-1.amazonaws.com/home");
+		redirectView.setUrl("http://cc-stockprofile-p2.com.s3-website-us-east-1.amazonaws.com/login");
 	    return redirectView;
 	}
 	

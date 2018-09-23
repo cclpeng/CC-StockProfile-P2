ac6fbff5077fbffe37514f5c44537059a4481027
Cindy <cpeng3030@gmail.com>
2018-09-23 10:01:40 -0700

    weird 405 postman, now is good...pushing i guess :O

--- src/main/java/com/revature/controllers/LoginController.java
+++ src/main/java/com/revature/controllers/LoginController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.view.RedirectView;
 
 import com.revature.models.User;
@@ -41,7 +42,7 @@ public class LoginController {
 //		return redirectView;
 //		
 //	}
-	@PostMapping(value="/login", consumes="application/json", produces=MediaType.APPLICATION_JSON_VALUE)
+	@PostMapping(consumes="application/json", produces=MediaType.APPLICATION_JSON_VALUE)
 	public RedirectView loginValidationRedirection(@RequestBody LoginForm loginForm)
 	{
 		

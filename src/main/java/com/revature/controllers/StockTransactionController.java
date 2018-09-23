9e5659afdd01f77fa7a8104b6b69df03ef532ca0
Cirey Francis <thecursedmerchant@gmail.com>
2018-09-22 20:32:31 -0400

    StockTransactions Work! Sorta....

--- src/main/java/com/revature/controllers/StockTransactionController.java
+++ src/main/java/com/revature/controllers/StockTransactionController.java
@@ -2,9 +2,15 @@ package com.revature.controllers;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,5 +28,23 @@ public class StockTransactionController {
 	public List<StockTransaction> findAllStockTransactions(){
 		return stockService.getStockTransactions();
 	}
+	
+	@GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public StockTransaction findStockById(@PathVariable("id") Long id) {
+		return stockService.getStockTransactionById(id);
+	}
+	
+	@PostMapping(consumes=MediaType.APPLICATION_JSON_VALUE)
+	public StockTransaction addStock(@Valid @RequestBody StockTransaction s) {
+		return stockService.createStockTransaction(s);
+	}
+	
+	@PutMapping(consumes=MediaType.APPLICATION_JSON_VALUE, produces=MediaType.APPLICATION_JSON_VALUE)
+	//@ResponseBody
+	public StockTransaction updateUser(@RequestBody StockTransaction s) {
+		return stockService.updateStockTransaction(s);
+	}
+	
+	
 
 }

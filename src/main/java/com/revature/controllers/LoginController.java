45d8eea3638312860b611f87bcc48936a12426b6
Cirey Francis <thecursedmerchant@gmail.com>
2018-09-22 22:54:16 -0400

    Post iiiiiiiiiiiiiiiiiiiiiiiitgit add .git add .

--- src/main/java/com/revature/controllers/LoginController.java
+++ src/main/java/com/revature/controllers/LoginController.java
@@ -2,22 +2,24 @@ package com.revature.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.view.RedirectView;
 
 import com.revature.services.UserService;
 
-@Controller
+@RestController
+@RequestMapping("/login")
 public class LoginController {
+	
 	@Autowired
 	UserService userService;
 	
-	
-	@GetMapping(value="/login")
+	@GetMapping
 	public RedirectView loginPage() {
 		//hehehhhh  :https://stackoverflow.com/questions/17955777/redirect-to-an-external-url-from-controller-action-in-spring-mvc
 		RedirectView redirectView = new RedirectView();
@@ -25,20 +27,19 @@ public class LoginController {
 	    return redirectView;
 	}
 	
-	@PostMapping(value="/login", consumes=MediaType.TEXT_PLAIN_VALUE, produces=MediaType.TEXT_PLAIN_VALUE)
-	@ResponseBody
+	@PostMapping
 //	public RedirectView loginValidationRedirection(@RequestBody String username, @RequestBody String password)
-	public String loginValidationRedirection(@RequestBody String username, @RequestBody String password)
+	public RedirectView loginValidationRedirection()
 	{
 		RedirectView redirectView = new RedirectView();
 		//set up a session with those params
 //		if(username.equals("cindy") && password.equals("pass"))
-//			redirectView.setUrl("http://cc-stockprofile-p2.com.s3-website-us-east-1.amazonaws.com/home");
+		redirectView.setUrl("http://cc-stockprofile-p2.com.s3-website-us-east-1.amazonaws.com/home");
 //		else
 //			redirectView.setUrl("http://cc-stockprofile-p2.com.s3-website-us-east-1.amazonaws.com/login");
 //		return redirectView;
 		
-		return "";
+		return redirectView;
 	}
 	
 }

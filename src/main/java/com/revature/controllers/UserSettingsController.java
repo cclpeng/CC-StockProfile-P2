24650f0d5374ccf517c7e5a23626649542f1d278
Cindy <cpeng3030@gmail.com>
2018-09-24 18:40:19 -0700

    updating usercontroller more

--- src/main/java/com/revature/controllers/UserSettingsController.java
+++ src/main/java/com/revature/controllers/UserSettingsController.java
@@ -1,30 +1,44 @@
 package com.revature.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.view.RedirectView;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.revature.models.User;
 import com.revature.models.forms.ChangeSettingsForm;
 import com.revature.services.UserService;
 
 @CrossOrigin
-@Controller
+@RestController
 @RequestMapping("/users")
 public class UserSettingsController {
 	
 	@Autowired
 	UserService userService;
 	
-	//doing PUT localhost:8080/users will do our post(into db) if we provide a body (like in postmaster)
+	//list all users with localhost:8080/users
+	@GetMapping(produces="application/json")
+	public List<User> listAllUsers()
+	{
+		return userService.listAllUsers();
+	}
+	
+	//post, create a user
+	@PostMapping(consumes="application/json", produces="application/json")
+	public User createUser(User newUser)
+	{
+		return userService.createUser(newUser);
+	}
+	
+	//put, update user
 	@PutMapping(consumes="application/json", produces="application/json")
-	@ResponseBody
 	public User changePassword(@RequestBody ChangeSettingsForm form) { 
 		User u = userService.verifyUser(form.getUsername(), form.getOldPassword());
 		if( u != null && (form.getNewPassword().equals( form.getConfirmPassword() )) )

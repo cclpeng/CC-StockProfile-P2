409302e2478b07745abc4feb3c71316ffa302cde
Cirey Francis <thecursedmerchant@gmail.com>
2018-09-26 16:07:03 -0400

    Did some cleaning up, SUPER PRETTY COOOOODDDEEE :D

--- src/main/java/com/revature/controllers/StockTransactionController.java
+++ src/main/java/com/revature/controllers/StockTransactionController.java
@@ -7,6 +7,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -46,6 +47,4 @@ public class StockTransactionController {
 		return stockService.updateStockTransaction(s);
 	}
 	
-	
-
 }

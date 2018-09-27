165f682e738ba9c81a074e45a9b5bdcbaf9351e9
Cindy <cpeng3030@gmail.com>
2018-09-27 10:47:49 -0700

    updating

--- src/test/java/com/revature/eval/java/core/UserTesting.java
+++ src/test/java/com/revature/eval/java/core/UserTesting.java
@@ -1,5 +1,37 @@
 package com.revature.eval.java.core;
 
-public class UserTesting {
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.revature.controllers.UserSettingsController;
+import com.revature.models.User;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class UserTesting {
+	@LocalServerPort
+	private int port;
+	
+	@Autowired
+	UserSettingsController uc;
+	
+	@Autowired
+	private TestRestTemplate restTemplate;
+	
+	@Test
+	public void returnUser() {
+		assertThat(this.restTemplate.getForObject("http://localhost:"+port+"/users", List.class)).isNotNull();
+//																				.isInstanceOf(object);
+	}
 }
